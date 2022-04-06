8729a9e508866f6b4415d3c7b0989f42b2e00a94
arushi1010 <arushi9b@gmail.com>
2022-04-06 11:54:37 -0700

    __init__.py

--- main.py
+++ main.py
@@ -1,11 +1,6 @@
 from flask import render_template
 from __init__ import app
 
-
-# @app.route('/')
-# def index():
-#     return render_template("base.html")
-
 @app.route('/')
 def index():
     return render_template("index.html")
