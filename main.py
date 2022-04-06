3fa9be898669de76a5a0fe4cbe87640dc304ae5e
Sarayu <sarayu.pr11@gmail.com>
2022-04-06 11:49:42 -0700

    home page

--- main.py
+++ main.py
@@ -1,10 +1,16 @@
 from flask import render_template
 from __init__ import app
 
+
+# @app.route('/')
+# def index():
+#     return render_template("base.html")
+
 @app.route('/')
 def index():
     return render_template("index.html")
- 
+
+
 if __name__ == "__main__":
     # runs the application on the repl development server
     app.run(debug=True, port="5222")
