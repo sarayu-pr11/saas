6b696168128833cb87016193113016ef35a5c4d7
Aadya Daita <aadyadaita@gmail.com>
2022-04-07 11:17:49 -0700

    attendance page running

--- main.py
+++ main.py
@@ -4,6 +4,10 @@ from __init__ import app
 @app.route('/')
 def index():
     return render_template("index.html")
+@app.route('/attendance')
+def attendance():
+    return render_template("attendance/attendance.html")
+
 
 
 
