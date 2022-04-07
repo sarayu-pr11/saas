a288b36c08e1062b03daf3ae5146ff972827d0c0
Aadya Daita <aadyadaita@gmail.com>
2022-04-07 11:09:04 -0700

    navbar running

--- main.py
+++ main.py
@@ -6,6 +6,7 @@ def index():
     return render_template("index.html")
 
 
+
 if __name__ == "__main__":
     # runs the application on the repl development server
     app.run(debug=True, port="5222")
